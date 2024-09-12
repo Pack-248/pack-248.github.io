1887c4035ee42f174aa551e7560ec14702d3212c
skaschimer <steve,kaschimer@slalom.com>
2024-09-12 08:37:22 -0400

    Initial website push

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Cub Scout Pack 248`,
+    siteUrl: `https://www.pack248.org`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+/**
+ * "gatsby-plugin-google-gtag", 
+ */
