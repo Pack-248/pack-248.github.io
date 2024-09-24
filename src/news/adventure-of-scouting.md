fb8a33d38623bfb3768544c7ee522ec274aa0ce2
skaschimer <steve,kaschimer@slalom.com>
2024-09-24 13:11:44 -0400

    updating resources page

---
+++ src/news/adventure-of-scouting.md
@@ -0,0 +1,82 @@
+---
+title: "The Adventure of Scouting"
+author: "Steve Kaschiner, Assistant Cubmaster"
+date: 9/24/2024
+imageurl: "../images/news/funwithpurpose.jpg"
+type: "News"
+---
+Learning by doing is a hallmark of outdoor education. Unit meetings offer information and knowledge used on outdoor adventures. A leader may describe and demonstrate a Scouting skill at a meeting, but the way Scouts truly learn an outdoor skill is to do it themselves on a unit outing.
+
+Service to others and good citizenship is learned through such outdoor activities as conservation projects, collecting food, building trails and shelters, and conducting community service projects that promote healthy living. Through helping other people, Scouts learn to appreciate how they can share themselves and their blessings to those in need. By giving service to benefit others, Scouts gain a sense of personal satisfaction.
+
+## A FAMILY PROGRAM TO GROW, LEARN, AND MAKE MEMORIES TOGETHER - START YOUR ADVENTURE TODAY
+
+Family involvement is essential to Cub Scouting's success. When we talk about "family" in Cub Scouting, we're sensitive to the realities of present-day families. Many Cub Scouts do not come from traditional two-parent homes. Some children live with a single parent or with other relatives or guardians. Cub Scouting considers a child's family to be the people with whom he or she lives.
+
+### Family Activities
+
+As a program for the entire family, Cub Scouting can help families teach their children a wholesome system of values and beliefs while building and strengthening relationships among family members.
+
+### Your Role as a Parent
+
+Cub Scouting helps parents and children grow closer, and your involvement as a parent of a Cub Scout is vital.
+
+### Becoming a Leader
+
+Volunteer leaders support Cub Scouting by serving in many roles, and often find great satisfaction in lending their support to youth and the community.
+
+### The BSA Family Award
+
+The BSA Family Award program offers activities to help strengthen all families—whether two-parent, single-parent, or nontraditional.
+
+Cub Scouting aims to develop youth into participating citizens of good character who are physically, spiritually, and mentally fit. The organization recognizes that it is the responsibility of parents and family to raise their children. The Cub Scout program is a resource that can help families teach their children a wholesome system of values and beliefs while building and strengthening relationships among family members.
+
+When we speak of the "family" in Cub Scouting, we are sensitive to the needs and structures of present-day families. Many Cub Scouts do not come from traditional two-parent homes. Some children live with a single parent or with other relatives or guardians. Cub Scouting considers a child's family to be the people with whom he or she lives.
+
+The family is probably the most effective mutual-help organization to be found. Family life has its good times and bad times, but, above all, it is people giving strength to one another when needed, people caring and letting it show, people leaning on one another, and people feeling loyal to one another. It's worth the effort to keep a family strong. For this reason, Cub Scouting seeks not only to help the child, but to unite and support the entire family.
+
+In turn, family involvement is vital to the success of the Cub Scout program. At this age, boys and girls are only beginning to discover their individuality—and as much as they seem to want to take on tasks and responsibilities on their own, they still look to their family for help and support. Family involvement provides that help and support for boys and girls, and it is positive reinforcement for the lessons learned in Cub Scouting.
+
+## New Family Orientation
+
+The pack leaders should provide an orientation session for new Cub Scout families to acquaint them with the program, its goals, procedures, and other basics. The information given to a new Cub Scout family should include:
+
+* An overview of Cub Scouting, including the program's aims and methods as well as policies and procedures
+* Details about the upcoming year, such as the dates of scheduled meetings and events, and information about the pack's newsletter, Web site, or other means of receiving updates
+* The procedures for joining a pack, helping with den activities, paying dues, and helping with the Scout's advancement
+* A review of the "Parent Guide" in the Scout's handbook, with an explanation of the ways parents or guardians work with their children on advancement
+* A review of "The Family's Responsibilities" as outlined in the _Cub Scout Leader Book,_ to let you know what is expected of each family
+* A copy of Cub Scouting's _BSA Family Activity Book,_ which explains how Cub Scouting can help meet family need
+* The family talent survey sheet, for you to identify ways in which your family can help the pack
+
+The new family orientation may take place before the family joins the pack or soon afterward. It's not uncommon for the orientation to be divided into two sessions: one before you have joined the pack, another afterward.
+
+## Family and Advancement
+
+The advancement program is part of the fun of Cub Scouting. To advance in rank, Scouts must complete certain activities, called 'adventures' or 'electives,' to earn each badge as they progress. A parent must sign the Cub Scout's handbook to certify that the child completed the activity. This is an excellent opportunity for families to get to know their children better. Family members and children get much satisfaction from it.
+
+Along the advancement trail, the family may be involved in many ways. Some achievements and electives require the Cub Scout to complete a project, with which most boys and girls will need help. Others require the Cub Scout to discuss or explain certain concepts or to demonstrate his ability to apply a skill, which will require the participation of family members.
+
+Most importantly, every achievement and elective in Cub Scouting requires a child to do their best. It's not necessary for the Cub Scout to do everything by themselves, and it is perfectly acceptable if they need some prompting to discuss or explain a concept. Sometimes, there can be a delicate balance between being too critical (which may damage a child's self-confidence) or too lenient (which can impair character development). The den leaders can help guide families to find a happy balance between expecting too much or too little, so that the program provides the maximum benefit to your child.
+
+## Pack Meetings and Events
+
+Weekly den meetings are intended for the children to interact with one another and their leaders in a small group setting. Any gathering on the pack level, however, is intended for Cub Scouts _and_ their families. Your family is not only welcome, but is expected to attend the monthly pack meetings, take part in special events such as the pinewood derby or the blue and gold banquet, and participate in the family camping program. Besides delivering the Cub Scout program, pack activities tend to be social events that bring together Scouting families in your community.
+
+At some events, parents and families may have a specific role. For example, when a Cub Scout has completed all requirements for a given badge, their parent presents the badge to them at the pack meeting. There are other times when you will play an active part in helping pack leaders to conduct the meeting. Even when neither of these things is strictly required of you, "just being there" at these events is important to your child's self-esteem. Your presence makes Cub Scouting all the more valuable to your child.
+
+## Parent-Leader Conferences
+
+Throughout the program year, parents will have many opportunities to meet with the den leader to discuss their child's participation and expectations of the den. The main purpose of these conferences is to help your child get the most from the Cub Scouting experience by coordinating the activities that happen in the den and in the home. Key topics for these conferences often include:
+
+* **Interactions with others**. The den leader can provide helpful information on your child's behavior in the company of other children outside the home.
+* **Advancement progress**. The conference is a chance to discuss the pace at which your child is progressing in their achievements and electives.
+* **Special needs**. You can indicate any special needs your child has, such as limitations, diet, medications, or health restrictions, so the den leader can plan activities accordingly.
+* **Program update**. The den leader can bring you up to date on the program, including the skills or topics to be covered at upcoming den meetings.
+* **Emergency procedures**. You and the den leader can review and update information such as emergency phone numbers, written medical permission, and any other information specific to your child's needs.
+
+Planned parent-leader conferences ensure that each of these topics, and any other item of importance, is discussed. However, if an issue or concern arises, it should be possible to arrange to talk with the den leader before the next scheduled conference.
+
+## Supporting Your Pack
+
+In various other ways, your family can pitch in to support your child's pack—by lending a hand at meetings, offering additional assistance with special events, helping to coordinate major activities, or assuming a leadership position in the pack. While none of this is strictly required of you, any support you can give the pack ultimately benefits your son or daughter. It's no coincidence that the packs that deliver the best program to the Cub Scouts are those that get the most support from families, who work together to make the pack a better, stronger organization.
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,10 +1,88 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab);
+library.add(fas);
 
 const ResourcesPage = () => {
   return (
     <Layout pageTitle="Resources">
-      
+      <h1 className='text-3xl font-bold mx-6'>Resources for Parents and Leaders</h1>
+      <div className='w-auto flex flex-rows gap-6 mx-6 my-6'>
+        <div id='leftSide' className='w-1/2 flex flex-col'>
+          <h2 className='text-2xl font-semibold m-6'>For Parents</h2>
+          <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>General</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/pdf/100-014_WEB.pdf' target='_blank' rel='noreferrer'>Cub Scout Parent's Guide</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scoutshop.org/blog/ultimate_resource_guide_for_new_scouting_parents.html' target='_blank' rel='noreferrer'>Ultimate Resource Guide for New Scouting Parents</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://scoutsmarts.com/support-child-cub-scouts/' target='_blank' rel='noreferrer'>Supporting Your Child In Cub Scouting: A Parent's Guide</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/cub-scout-uniform/' target='_blank' rel='noreferrer'>Uniform Insignia</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Guide to uniform patches and placement</span></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://boyslife.org/' target='_blank' rel='noreferrer'>Scout Life Magazine Website</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://scoutingmagazine.org/' target='_blank' rel='noreferrer'>Scouting Magazine Website</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'folder']}/> <a href='https://drive.google.com/drive/folders/1HUli1anmDRvYQ_p4HjDKg6pMhmmsZMWv?usp=drive_link' target='_blank' rel='noreferrer'>Pack 248 Parent Google Drive</a></li>
+          </ul>
+          <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>Membership</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/pdf/524-501.pdf' target='_blank' rel='noreferrer'>Application - Adult</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Application form for Adult Leader</span></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/pdf/524-406.pdf' target='_blank' rel='noreferrer'>Application - Youth</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Application form for Cub Scouts</span></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://michiganscouting.org/trial-membership/' target='_blank' rel='noreferrer'>Cub Scout Trial Membership</a><br/><span className='text-scoutDarkGray ml-6 italic text-sm'>We have options that will allow you and your youth the ability to learn how amazing the Cub Scout program can be</span></li>
+          </ul>
+        </div>
+        <div id='rightSide' className='w-1/2 flex flex-col'>
+          <h2 className='text-2xl font-semibold m-6'>For Leaders</h2>
+          <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>Scouting Safely</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/pdf/19-127WB_Singles.pdf' target='_blank' rel='noreferrer'>First Aid Log</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Utilize this log for your unit activities to document health and first aid issues and the interventions provided.</span></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/healthsafety/pdf/680-027.pdf' target='_blank' rel='noreferrer'>SAFE Service Project Planning Checklist</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>For service projects, lean on this checklist to help develop a detailed plan and execute your endeavors.</span></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/health-and-safety/incident-report/' target='_blank' rel='noreferrer'>Incident Reporting FAQs</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/HealthSafety/pdf/680-685.pdf' target='_blank' rel='noreferrer'>Age Appropriate Guidelines</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>This chart indicates what activities your Cub Scouts are allowed to participate in.</span></li>
+            </ul>
+          <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>Den Info</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/lion-resources/' target='_blank' rel='noreferrer'>Lion Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/tiger-resources/' target='_blank' rel='noreferrer'>Tiger Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/wolf-resources/' target='_blank' rel='noreferrer'>Wolf Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/bear-resources/' target='_blank' rel='noreferrer'>Bear Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/webelos-resources/' target='_blank' rel='noreferrer'>Webelos Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/aol-den-leader-resources/' target='_blank' rel='noreferrer'>Arrow of Light Resources</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/programs/cub-scouts/den-meeting-resources/den-leader-tips-tricks-video-series/' target='_blank' rel='noreferrer'>Den Leader Tips and Tricks Videos</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://advancements.scouting.org/' target='_blank' rel='noreferrer'>Internet Advancement</a></li>
+            <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'folder']}/> <a href='https://drive.google.com/drive/folders/1u5N-Nq0uvRWmyTqAHXmAxCHf4S0iPtfe?usp=drive_link' target='_blank' rel='noreferrer'>Pack 248 Leader Google Drive</a></li>
+          </ul>
+          <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>Training</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://my.scouting.org/' target='_blank' rel='noreferrer'>Den Leader Position-Specific Training</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Before you conduct a den meeting, complete the online training for Den Leader</span></li>
+          </ul>
+        </div>
+      </div>
+      <div className='w-full border- mx-6 flex flex-col'>
+        <h2 className='text-2xl font-semibold m-6'>For Both</h2>
+        <ul className='mx-6 mb-6'>
+          <li className='font-semibold text-scoutWarmGray'>Scouting Safely</li>
+          <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue'/>
+          <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/training/youth-protection/' target='_blank' rel='noreferrer'>Scouting's Barriers to Abuse/Youth Protection</a></li>
+          <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/HealthSafety/pdf/680-001_AB.pdf' target='_blank' rel='noreferrer'>Medical A & B Forms</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Applies to ALL participants—in basic Scouting activities such as local tours and weekend camping trips less than 72 hours in duration.</span></li>
+          <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/HealthSafety/pdf/680-001_ABC.pdf' target='_blank' rel='noreferrer'>Medical A, B & C Forms</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>A pre-participation physical is needed for: Long Term Camps, Wood Badge, Jamborees, Events 72 hours or more</span></li>
+          <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://www.scouting.org/wp-content/uploads/2021/06/680-11421_SAFE.pdf' target='_blank' rel='noreferrer'>SAFE Checklist</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>To ensure the safety of participants, the Boy Scouts of America expects leaders to use the four points of SAFE when delivering the Scouting program</span></li>
+          <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://www.scouting.org/health-and-safety/gss/' target='_blank' rel='noreferrer'>Guide to Safe Scouting (GTSS)</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>A cornerstone resource for everyone in Scouting, this guide offers information on the latest policies and procedures, serving as your go-to manual to help you plan and provide a safe Scouting program</span></li>
+          <li className='text-cubScoutBlue'><FontAwesomeIcon icon={['fas', 'file']}/> <a href='https://filestore.scouting.org/filestore/HealthSafety/pdf/Scouter_Code_of_Conduct.pdf' target='_blank' rel='noreferrer'>Scouter Code of Conduct</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>For adults engaging in Scouting, this document serves as a standard, illustrating the behavior that nurtures a positive, supportive environment.</span></li>
+          </ul>
+        <ul className='mx-6 mb-6'>
+            <li className='font-semibold text-scoutWarmGray'>Scout Management</li>
+            <hr className='border-1 border-cubScoutDarkBlue fill-cubScoutDarkBlue mx-6'/>
+            <li className='text-cubScoutBlue mt-2'><FontAwesomeIcon icon={['fas', 'globe']}/> <a href='https://scoutbook.scouting.org/' target='_blank' rel='noreferrer'>Scoutbook</a><br/><span className='text-sm text-scoutDarkGray italic ml-6'>Our Pack uses Scoutbook to handle and track all of our scout's advancement.</span></li>
+            <li className='text-cubScoutBlue'>Scouting App<br/><span className='text-sm text-scoutDarkGray italic ml-6'>App is built just for Scouts and their parents and pulls existing advancement data straight from Scoutbook.<br/><FontAwesomeIcon className='ml-6 text-cubScoutBlue' icon={['fas', 'globe']}/> <a className='text-cubScoutBlue' href='https://play.google.com/store/apps/details?id=com.bsa.sbm&pli=1' target='_blank' rel='noreferrer'>Scouting - Apps on Google Play</a><br/><FontAwesomeIcon className='ml-6 text-cubScoutBlue' icon={['fas', 'globe']}/> <a  className='text-cubScoutBlue' href='https://apps.apple.com/us/app/scouting/id1218464473' target='_blank' rel='noreferrer'>Scouting on the App Store (Apple)</a></span></li>
+          </ul>
+      </div>
     </Layout>
   )
 }
