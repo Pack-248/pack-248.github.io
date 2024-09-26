fddab1e01c8a514d97bb0b24d63c27c4b268bf92
skaschimer <steve,kaschimer@slalom.com>
2024-09-26 19:33:53 -0400

    added more quotes

--- src/utils.js
+++ src/utils.js
@@ -34,6 +34,57 @@ export function GetRandomScoutQuote() {
         {
             quote: "Scout motto: Do your best!"
         },
+        {
+            quote: "The Aims of Scouting are: Character, Citizenship, Personal Fitness, and Leadership"
+        },
+        {
+            quote: "The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law."
+        },
+        {
+            quote: "The Methods of Scouting are: Living the Ideals, Belonging to a Den, Advancement, Family Involvement, Activities, Serving the Neighborhood, and Uniform."
+        },
+        {
+            quote: "A Scout is never taken by surprise; he knows exactly what to do when anything unexpected happens. - Lord Robert Baden-Powell"
+        },
+        {
+            quote: "A week of camp life is worth six months of theoretical teaching in the meeting room. - Lord Robert Baden-Powell"
+        },
+        {
+            quote: "Boys can see adventure in a dirty old duck puddle, and if the Scoutmaster is a boys' man he can see it too. - Lord Robert Baden-Powell"
+        },
+        {
+            quote: "Nobody is too small to do a good turn, even if it is only to smile. - Lord Robert Baden Powell"
+        },
+        {
+            quote: "Be Prepared… the meaning of the motto is that a Scout must prepare himself by previous thinking out and practicing how to act on any accident or emergency so that he is never taken by surprise. - Lord Robert Baden Powell"
+        },
+        {
+            quote: "A Good Turn will educate the Scout out of the groove of selfishness. - Lord Robert Baden Powell"
+        },
+        {
+            quote: "If you make listening and observation your occupation you will gain much more than you can by talk. - Lord Robert Baden Powell"
+        },
+        {
+            quote: "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine. -Neil Armstrong"
+        },
+        {
+            quote: "As we look ahead into the next century, leaders will be those who empower others. -Bill Gates"
+        },
+        {
+            quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach some of us more that what we could learn from books. -John Lubbock"
+        },
+        {
+            quote: "Now I know the secret of making the best persons; it is to grow in the open air and to eat and sleep with the Earth. -Walt Whitman"
+        },
+        {
+            quote: "The glories of a mountain campfire are far greater than may be guessed…. One can make a day of any size, and regulate the rising and setting of his own sun and the brightness of its shining. -John Muir"
+        },
+        {
+            quote: "It only takes a few years of dedication to become an Eagle Scout, but the lessons learned in Scouting will be carried for the rest of your life."
+        },
+        {
+            quote: "It only takes a few years of dedication to become an Eagle Scout, but the lessons learned in Scouting will be carried for the rest of your life."
+        }
     ];
 
     const total = quotes.length;
