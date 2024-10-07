0b369402dedfd8be9dcc02323f0b602b288ddd20
skaschimer <steve,kaschimer@slalom.com>
2024-10-07 16:31:16 -0400

    add new page for recruiting

---
+++ src/pages/recruiting.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const RecruitingPage = () => {
+    return (
+        <Layout pageTitle="Recruiting">
+
+        </Layout>
+    )
+};
+
+export const Head = () => (
+    <>
+        <title>Pack 248 - Join Us!</title>
+        <meta name="description" content="Pack 248 is a Family Cub Scout Pack located in Clinton Township, Michigan" />
+    </>
+);
+
+export default RecruitingPage;
